94ec73df5d541af916a9e5c55fa3482a3f0e502f
ZERO <104218898+PrimeCoderZERO@users.noreply.github.com>
2023-12-27 00:22:02 +0300

    Add files via upload

---
+++ Verify _ ІПЗ-110/config.py
@@ -0,0 +1,15 @@
+config = {
+	"token": "MTEwNTA4NDI1NzUzOTQ3MzQxOA.Gy0b87.JjCxM3zXssn2toTGtHXpIZVvX8ehGNazquqAWc",
+	"print": "123",
+	"man": "1114919360386056203",
+	"girl": "1116043638074523779",
+	"otzivi": "1188403041058177104",
+	"logi": "1188401430399291422",
+	"unverify": "1116043649826951178",
+	"nedopusk": "1116043647432015883",
+	"support": "1116043648849674321",
+	"guild_id": "1079402341230465125",
+	"server_name": "test bot",
+	"dot": "<:3133113:1160698301163765760> "
+
+}
